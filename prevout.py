3339f8c28d76c98a06c587d483065e3147894a94
Madame President <64007837+madame-president@users.noreply.github.com>
2024-07-04 10:36:28 -0600

    Delete prevout.py

--- prevout.py
+++
@@ -1,9 +0,0 @@
-# Checks if bitcoin_address is found in the tx inputs' prevouts.
-
-def search_prevouts(prevouts, bitcoin_address):
-    return any(
-      prevout['scriptpubkey_address'] == bitcoin_address 
-      for prevout in prevouts 
-      if 'scriptpubkey_address' in prevout
-    )
-    pass
